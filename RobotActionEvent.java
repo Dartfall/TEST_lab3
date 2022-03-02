6b181abb315baf701d6f2a7078ed2ecf64fdd261
Dartfall <megutron@mail.ru>
2022-03-02 13:39:08 +0300

    karpenko: commit 3

--- RobotActionEvent.java
+++ RobotActionEvent.java
@@ -4,6 +4,8 @@ import java.util.EventObject;
 
 //add new action
 
+//add do samthing
+
 public class RobotActionEvent extends EventObject {
     public RobotActionEvent(Object source) { 
         super(source); 
