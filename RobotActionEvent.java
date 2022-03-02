1d42a382997f2bd6a47efbbcc01c66e3c5cb74a0
Dartfall <megutron@mail.ru>
2022-03-02 13:10:12 +0300

    karpenko: commit 1

--- RobotActionEvent.java
+++ RobotActionEvent.java
@@ -2,6 +2,8 @@ package brokenrobotgame.model.events;
 
 import java.util.EventObject;
 
+//add new action
+
 public class RobotActionEvent extends EventObject {
     public RobotActionEvent(Object source) { 
         super(source); 
